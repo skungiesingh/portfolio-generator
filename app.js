f1be9d9d26957a91a59aaaf482b702b61b13e102
Shastri Kungiesingh <ssppksingh347@gamil.com>
2021-04-23 03:41:12 -0400

    update

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Hello Node!');
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+//const printProfileData = profileDataArr => {
+//    this
+//    for (let i = 0; i < profileDataArr.length; i += 1) {
+//    console.log(profileDataArr[i]);
+//    }
+
+ //   console.log('============');
+
+//    is the same as this...
+//    profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
+
+//printProfileData(profileDataArgs);
